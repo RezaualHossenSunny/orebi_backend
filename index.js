326c670a461c1b2e934594d00bef4be2edd02591
RezaualHossenSunny <91420025+RezaualHossenSunny@users.noreply.github.com>
2024-03-17 15:52:13 +0600

    cros add

--- index.js
+++ index.js
@@ -2,7 +2,9 @@ const express = require('express')
 const dbConnetion = require('./config/dbConection')
 const app = express();
 const userList=require('./models/userScema');
+var cors = require('cors')
 app.use(express.json());
+app.use(cors())
 require('dotenv').config();
 const route  = require('./Router');
 
